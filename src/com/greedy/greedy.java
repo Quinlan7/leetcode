58965bd9db04bdc46e826134ce01cf98236e53a2
quin-lan <1609353139@qq.com>
2024-01-23 11:47:54 +0800

    贪婪算法2

--- src/com/greedy/greedy.java
+++ src/com/greedy/greedy.java
@@ -105,4 +105,87 @@ public class greedy {
         return ret;
     }
 
+
+    /**
+     * 122：买卖股票的最佳时机 II
+     * @param prices
+     * @return
+     */
+    public int maxProfit(int[] prices) {
+        int inPrice = prices[0],sum = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if(prices[i] > inPrice){
+                sum += prices[i] - inPrice;
+            }
+            inPrice = prices[i];
+        }
+        return sum;
+    }
+
+
+    /**
+     * 55：跳跃游戏
+     * @param nums
+     * @return
+     */
+    public boolean canJump(int[] nums) {
+        if(nums.length == 1) return true;
+        int max=0;
+        for (int i = 0; i < nums.length-1; i++) {
+            if(i > max) return false;
+            if(i + nums[i] >= nums.length-1) return true;
+            max = Math.max(i + nums[i],max);
+        }
+        return false;
+    }
+
+
+    /**
+     * 45:跳跃游戏II
+     * @param nums
+     * @return
+     */
+    int count_45 = 0;
+    public int jump(int[] nums) {
+        jumpStep(nums,nums.length-1);
+        return count_45;
+    }
+
+    private void jumpStep(int[] nums, int i) {
+        if(i == 0)return;
+        int min = i;
+        for (int j = i-1; j >= 0; j--) {
+            if(nums[j] + j >= i) min = j;
+        }
+        count_45++;
+        jumpStep(nums,min);
+    }
+
+
+    /**
+     * 1005:k次取反后最大化的数组和
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int largestSumAfterKNegations(int[] nums, int k) {
+        Arrays.sort(nums);
+        for (int i = 0; i < k; i++) {
+            if(i>nums.length-1){
+                Arrays.sort(nums);
+                if((k-i) % 2 ==1) nums[0] = -nums[0];
+                break;
+            }
+            if(nums[i]<0){
+                nums[i] = Math.abs(nums[i]);
+            }else {
+                Arrays.sort(nums);
+                if((k-i) % 2 ==1) nums[0] = -nums[0];
+                break;
+            }
+        }
+        return Arrays.stream(nums).sum();
+    }
+
+
 }
