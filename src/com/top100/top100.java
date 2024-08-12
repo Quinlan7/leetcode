c45c6d2aede641e214f2e8e8b04b7ac59113ee7e
Quinlan7 <1609353139@qq.com>
2024-08-12 22:45:00 +0800

    top100 finish real

--- src/com/top100/top100.java
+++ src/com/top100/top100.java
@@ -2097,6 +2097,31 @@ public class top100 {
     }
 
 
+    /**
+     * 238:除自身以外数组的乘机
+     * @param nums
+     * @return
+     */
+    public int[] productExceptSelf(int[] nums) {
+        int[] ret = new int[nums.length];
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+        left[0] = nums[0];
+        right[nums.length-1] = nums[nums.length - 1];
+        for(int i = 1 ; i< nums.length ; i++ ){
+            left[i] = left[i-1] * nums[i];
+            right[nums.length - i -1] = right[nums.length - i] * nums[nums.length - i -1];
+        }
+        ret[0] = right[1];
+        ret[nums.length - 1] = left[nums.length - 2];
+        for(int i = 1 ; i< nums.length - 1 ; i++ ){
+            ret[i] = left[i-1] * right[i+1];
+        }
+        return ret;
+
+    }
+
+
 
     
     
